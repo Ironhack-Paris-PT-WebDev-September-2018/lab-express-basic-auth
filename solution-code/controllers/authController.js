da1726318601dc4db8b7303dd5b56bb81ca46a47
ta-webpt-par <eloji9@hotmail.com>
2019-01-08 19:31:17 +0100

    solution commit

---
+++ solution-code/controllers/authController.js
@@ -0,0 +1,79 @@
+const authController = require("express").Router();
+
+// User model
+const User           = require("../models/users");
+
+// BCrypt to encrypt passwords
+const bcrypt         = require("bcrypt");
+const bcryptSalt     = 10;
+
+authController.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+authController.post("/signup", (req, res, next) => {
+  var username = req.body.username;
+  var userpass = req.body.password;
+
+  if (username === "" || userpass === "") {
+    res.render("auth/signup", { errorMessage: "Indicate a username and a password to sign up" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", { errorMessage: "The username already exists" });
+      return;
+    }
+
+    var salt     = bcrypt.genSaltSync(bcryptSalt);
+    var password = bcrypt.hashSync(userpass, salt);
+
+    var newUser  = User({
+      username,
+      password
+    });
+
+    newUser.save((err) => {
+      res.redirect("/login");
+    });
+  });
+});
+
+authController.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
+authController.post("/login", (req, res, next) => {
+  var username = req.body.username;
+  var userpass = req.body.password;
+
+  if (username === "" || userpass === "") {
+    res.render("auth/login", { errorMessage: "Indicate username and password to sign up" });
+    return;
+  }
+
+  User.findOne({ username }, "_id username password", (err, user) => {
+    if (err || !user) {
+      res.render("auth/login", { errorMessage: "The username doesn't exist" });
+    } else {
+      if (bcrypt.compareSync(userpass, user.password)) {
+        req.session.currentUser = user;
+        res.redirect("/");
+      } else {
+        res.render("auth/login", { errorMessage: "Incorrect password" });
+      }
+    }
+  });
+});
+
+authController.get("/logout", (req, res, next) => {
+  if (!req.session.currentUser) { res.redirect("/login"); return; }
+
+  req.session.destroy((err) => {
+    if (err) { console.log(err); }
+    else { res.redirect("/login"); }
+  });
+});
+
+module.exports = authController;
